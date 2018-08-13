4986640c4dc54c2e60d30492802372af72f0c6d3
Kevin Corbin <kecorbin@cisco.com>
2018-08-13 17:14:27 -0500

    add setup.sh / update README

--- gitlab/README.md
+++ gitlab/README.md
@@ -2,10 +2,20 @@
 
 Quickly spin up a GitLab stack in your sandbox.
 
-# Usage
+# Quickstart
 
 ```
 git clone https://github.com/DevNetSandbox/sbx_multi_ios.git
 cd sbx_multi_ios/gitlab
-docker-compose up -d
+./setup.sh
 ```
+
+Once the process is complete, GitLab should be accessible at http://10.10.20.20 The
+default credentials are `developer/C1sco12345`
+
+# Troubleshooting / Verification
+
+Should you need to tinker with the admin settings you can use the `root` account with
+password `C1sco12345`
+
+You can verify the runner registered successfully at http://10.10.20.20/admin/runners
---
+++ gitlab/setup.sh
@@ -0,0 +1,62 @@
+#!/usr/bin/env bash
+
+gitlab_host="http://10.10.20.20"
+gitlab_user="root"
+gitlab_password="C1sco12345"
+
+# create gitlab personal access token
+# https://gist.github.com/michaellihs/5ef5e8dbf48e63e2172a573f7b32c638
+create_gitlab_token () {
+  # curl for the login page to get a session cookie and the sources with the auth tokens
+  body_header=$(curl -c /tmp/cookies.txt -i "${gitlab_host}/users/sign_in" -s)
+  # grep the auth token for the user login for
+  #   not sure whether another token on the page will work, too - there are 3 of them
+  csrf_token=$(echo $body_header | perl -ne 'print "$1\n" if /new_user.*?authenticity_token"[[:blank:]]value="(.+?)"/' | sed -n 1p)
+  # send login credentials with curl, using cookies and token from previous request
+  curl --silent -b /tmp/cookies.txt -c /tmp/cookies.txt -i "${gitlab_host}/users/sign_in" \
+      --data "user[login]=${gitlab_user}&user[password]=${gitlab_password}" \
+      --data-urlencode "authenticity_token=${csrf_token}"
+
+  # send curl GET request to personal access token page to get auth token
+  body_header=$(curl --silent -H 'user-agent: curl' -b /tmp/cookies.txt -i "${gitlab_host}/profile/personal_access_tokens" -s)
+  csrf_token=$(echo $body_header | perl -ne 'print "$1\n" if /authenticity_token"[[:blank:]]value="(.+?)"/' | sed -n 1p)
+
+  # curl POST request to send the "generate personal access token form"
+  # the response will be a redirect, so we have to follow using `-L`
+  body_header=$(curl --silent -L -b /tmp/cookies.txt "${gitlab_host}/profile/personal_access_tokens" \
+      --data-urlencode "authenticity_token=${csrf_token}" \
+      --data 'personal_access_token[name]=golab-generated&personal_access_token[expires_at]=&personal_access_token[scopes][]=api')
+
+  personal_access_token=$(echo $body_header | perl -ne 'print "$1\n" if /created-personal-access-token"[[:blank:]]value="(.+?)"/' | sed -n 1p)
+
+}
+
+# prints colored text
+success () {
+    COLOR="92m"; # green
+    STARTCOLOR="\e[$COLOR";
+    ENDCOLOR="\e[0m";
+    printf "$STARTCOLOR%b$ENDCOLOR" "done\n";
+}
+
+echo ""
+printf "Launching Gitlab CE ..."
+docker-compose up -d 2>> gitlab_setup.log
+success
+
+printf "Waiting for Gitlab CE to become available ."
+
+until $(curl --output /dev/null --silent --head --fail http://10.10.20.20); do
+    printf '.'
+    sleep 10
+done
+success
+
+printf "Registering GitLab Runner ... "
+
+docker-compose exec runner1 gitlab-runner register 2>&1 >> gitlab_setup.log
+success
+printf "Creating user 'developer' ..."
+create_gitlab_token 2>&1 >> gitlab_setup.log
+curl -s --header "PRIVATE-TOKEN: $personal_access_token" -d "email=developer@devnetsandbox.cisco.com&password=C1sco12345&username=developer&name=developer&skip_confirmation=true" "http://10.10.20.20/api/v4/users" 2>&1 >> gitlab_setup.log
+success
