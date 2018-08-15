8c913b86c217e269fb3ffe433123c5e90d9d1d9a
Kevin Corbin <kecorbin@cisco.com>
2018-08-14 22:59:01 -0500

    add note about deleting repo

--- network-cicd/cleanup.sh
+++ network-cicd/cleanup.sh
@@ -5,9 +5,12 @@ ncs-setup --reset
 ncs-netsim stop
 ncs-netsim delete-network
 rm -rf netsim inventory.yaml packages state target scripts logs ncs-cdb storedstate README.netsim README.ncs ncs.conf
-
+rm -Rf .git
 
 cd $base_dir/virl/test/
 virl down
 cd $base_dir/virl/prod/
 virl down
+
+
+echo "Cleanup complete, you may want to also delete any repositories on the gitlab server"
