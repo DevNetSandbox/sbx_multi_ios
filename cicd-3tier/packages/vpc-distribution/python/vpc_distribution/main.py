5c45aec837c836d8e1d10e8d0c85dab092e79925
Kevin Corbin <kecorbin@cisco.com>
2018-10-19 21:49:34 -0500

    generate initial package skeleton

---
+++ cicd-3tier/packages/vpc-distribution/python/vpc_distribution/main.py
@@ -0,0 +1,88 @@
+# -*- mode: python; python-indent: 4 -*-
+import ncs
+from ncs.application import Service
+from ncs.dp import Action
+
+
+# ---------------
+# ACTIONS EXAMPLE
+# ---------------
+class DoubleAction(Action):
+    @Action.action
+    def cb_action(self, uinfo, name, kp, input, output):
+        self.log.info('action name: ', name)
+        self.log.info('action input.number: ', input.number)
+
+        # Updating the output data structure will result in a response
+        # being returned to the caller.
+        output.result = input.number * 2
+
+
+# ------------------------
+# SERVICE CALLBACK EXAMPLE
+# ------------------------
+class ServiceCallbacks(Service):
+
+    # The create() callback is invoked inside NCS FASTMAP and
+    # must always exist.
+    @Service.create
+    def cb_create(self, tctx, root, service, proplist):
+        self.log.info('Service create(service=', service._path, ')')
+
+        vars = ncs.template.Variables()
+        vars.add('DUMMY', '127.0.0.1')
+        template = ncs.template.Template(service)
+        template.apply('vpc-distribution-template', vars)
+
+    # The pre_modification() and post_modification() callbacks are optional,
+    # and are invoked outside FASTMAP. pre_modification() is invoked before
+    # create, update, or delete of the service, as indicated by the enum
+    # ncs_service_operation op parameter. Conversely
+    # post_modification() is invoked after create, update, or delete
+    # of the service. These functions can be useful e.g. for
+    # allocations that should be stored and existing also when the
+    # service instance is removed.
+
+    # @Service.pre_lock_create
+    # def cb_pre_lock_create(self, tctx, root, service, proplist):
+    #     self.log.info('Service plcreate(service=', service._path, ')')
+
+    # @Service.pre_modification
+    # def cb_pre_modification(self, tctx, op, kp, root, proplist):
+    #     self.log.info('Service premod(service=', kp, ')')
+
+    # @Service.post_modification
+    # def cb_post_modification(self, tctx, op, kp, root, proplist):
+    #     self.log.info('Service premod(service=', kp, ')')
+
+
+# ---------------------------------------------
+# COMPONENT THREAD THAT WILL BE STARTED BY NCS.
+# ---------------------------------------------
+class Main(ncs.application.Application):
+    def setup(self):
+        # The application class sets up logging for us. It is accessible
+        # through 'self.log' and is a ncs.log.Log instance.
+        self.log.info('Main RUNNING')
+
+        # Service callbacks require a registration for a 'service point',
+        # as specified in the corresponding data model.
+        #
+        self.register_service('vpc-distribution-servicepoint', ServiceCallbacks)
+
+        # When using actions, this is how we register them:
+        #
+        self.register_action('vpc-distribution-action', DoubleAction)
+
+        # If we registered any callback(s) above, the Application class
+        # took care of creating a daemon (related to the service/action point).
+
+        # When this setup method is finished, all registrations are
+        # considered done and the application is 'started'.
+
+    def teardown(self):
+        # When the application is finished (which would happen if NCS went
+        # down, packages were reloaded or some error occurred) this teardown
+        # method will be called.
+
+        self.log.info('Main FINISHED')
