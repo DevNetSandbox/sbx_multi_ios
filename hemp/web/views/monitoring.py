dacc81b456a0275fc5490c0a65744198d2795bfb
Kevin Corbin <kecorbin@cisco.com>
2018-11-01 14:14:32 -0500

    fix grafana iframe

--- hemp/web/views/monitoring.py
+++ hemp/web/views/monitoring.py
@@ -1,5 +1,10 @@
-from flask import render_template
+from flask import request, render_template
 
 
 def monitoring():
-    return render_template('monitoring.html')
+    host = request.host
+    if ':' in host:
+        host = host.split(':')[0]
+    grafana_host = host + ":3000"
+    return render_template('monitoring.html',
+                           grafana_host=grafana_host)
