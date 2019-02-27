168f583c59e5fa433a1fac4efc4f18335a6cbf32
Kevin Corbin <kecorbin@cisco.com>
2019-02-26 22:52:29 -0600

    lock down some ansible versions

--- dmvpn-as-code/nso_setup.sh
+++ dmvpn-as-code/nso_setup.sh
@@ -1,11 +1,11 @@
 if [ "$HOSTNAME" = devbox ]; then
-  echo "Doing some quick cleanup for sandbox environment"
+  echo "Doing some quick reconfiguration of the devbox"
   # update symlink to desired nso version
   sudo rm /opt/nso
   sudo ln -s /opt/nso47 /opt/nso
   source /opt/nso/ncsrc
 
-  # workaround - sbx needs updating
+  # workarounds - sbx needs updating?
   cur_dir=$(pwd)
   sudo mv /opt/nso/packages/services/ncs-4.6-resource-manager-project-3.3.0/packages/*.tar.gz \
           /opt/nso/packages/services/
@@ -14,6 +14,12 @@ if [ "$HOSTNAME" = devbox ]; then
   sudo rm *.tar.gz
   sudo rm -rf /opt/nso/packages/services/ncs-4.6-resource-manager-project-3.3.0/
   cd $cur_dir
+  sudo mv /opt/nso/etc/ncs/ncs.conf /opt/nso/etc/ncs/ncs.conf.bak
+  sudo sh -c 'grep -v "<dir>/opt/nso/packages/neds/</dir>" /opt/nso/etc/ncs/ncs.conf.bak > /opt/nso/etc/ncs/ncs.conf'
+
+  # if we are going to maintain ansible in the python system install we need to make sure
+  # it's keeping up to date
+  sudo pip uninstall ansible
 fi
 
 # make sure dependencies are installed and virtualenv is activated
