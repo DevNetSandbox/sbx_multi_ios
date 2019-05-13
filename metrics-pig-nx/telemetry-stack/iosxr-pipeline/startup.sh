b57ad5ca1fd4b8962efea61f6dbc0a513461d869
Chad Peterson <chapeter@cisco.com>
2019-05-12 21:55:47 -0500

    pig-nx stack

--- metrics-pig-nx/telemetry-stack/iosxr-pipeline/startup.sh
+++
@@ -1,6 +0,0 @@
-#! /bin/bash
-# curl -G http://$influx_ip:$influx_port/query --data-urlencode 'q=CREATE DATABASE "netdevops"'
-# curl -G http://$influx_ip:$influx_port/query?q=CREATE+USER+%22cisco%22+WITH+PASSWORD+'cisco'&db=_internal
-# curl -G http://$influx_ip:$influx_port/query --data-urlencode 'q=GRANT ALL ON netdevops TO cisco'
-# #./pipeline -config=data/config/pipeline.conf_REWRITTEN -pem=./id_rsa -log= -debug
-./pipeline -config=/etc/pipeline/pipeline.conf -pem=/etc/pipeline/id_rsa -log= -debug
