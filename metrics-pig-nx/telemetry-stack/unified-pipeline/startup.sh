4d1ccbfc11d44a52e630844f446a7dad20255ded
Chad Peterson <chapeter@cisco.com>
2019-05-17 12:06:34 -0500

    clean up old naming

--- metrics-pig-nx/telemetry-stack/unified-pipeline/startup.sh
+++
@@ -1,4 +0,0 @@
-curl -G http://$influx_ip:$influx_port/query --data-urlencode 'q=CREATE DATABASE "netdevops"'
-curl -G http://$influx_ip:$influx_port/query?q=CREATE+USER+%22cisco%22+WITH+PASSWORD+'cisco'&db=_internal
-curl -G http://$influx_ip:$influx_port/query --data-urlencode 'q=GRANT ALL ON netdevops TO cisco'
-./pipeline -config=/etc/pipeline/pipeline.conf -pem=/etc/pipeline/id_rsa -log= -debug
